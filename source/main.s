bb82f5733a74add215cbf6b8fbeb20cfccb09330
Max Heinritz <maxheinritz@gmail.com>
2013-09-21 16:30:48 -0700

    lesson 3 clean

--- source/main.s
+++ source/main.s
@@ -17,34 +17,20 @@ bl SetGpioFunction
 .unreq pinNum
 .unreq pinFunc
 
+pinNum .req r0
+pinVal .req r1
+
 loop$:
 	// Turn the LED on.
-	pinNum .req r0
-	pinVal .req r1
 	mov pinNum,#16
 	mov pinVal,#0
 	bl SetGpio
-	.unreq pinNum
-	.unreq pinVal
-
-	mov r2,#0x3F0000
-	wait1$:
-		sub r2,#1
-		cmp r2,#0
-		bne wait1$
+	bl Pause
 
 	// Turn the LED off.
-	pinNum .req r0
-	pinVal .req r1
 	mov pinNum,#16
 	mov pinVal,#1
 	bl SetGpio
-	.unreq pinNum
-	.unreq pinVal
+	bl Pause
 
-	mov r2,#0x3F0000
-	wait2$:
-		sub r2,#1
-		cmp r2,#0
-		bne wait2$
 b loop$
