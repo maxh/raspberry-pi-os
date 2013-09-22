4dc3a6905c6f697b1c51ba2246bed36dccd28895
Max Heinritz <maxheinritz@gmail.com>
2013-09-22 15:01:44 -0700

    morse code working

--- source/main.s
+++ source/main.s
@@ -17,24 +17,46 @@ bl SetGpioFunction
 .unreq pinNum
 .unreq pinFunc
 
+ptrn .req r8
+ldr ptrn,=pattern // Set ptrn equal to pattern, which is a memory address.
+ldr ptrn,[ptrn] // Load ptrn w/ the value at the address currently in ptrn.
+seq .req r7
+mov seq,#0
+
 pinNum .req r0
 pinVal .req r1
 delay .req r2
-	
+
 loop$:
-	// Turn the LED on.
-	mov pinNum,#16
+	// Set the PIN value according to the sequence.
 	mov pinVal,#0
-	bl SetGpio
-	mov delay,#0x3F0000
-	bl Timer
-
-	// Turn the LED off.
+	mov r2,#1
+	lsl r2,seq
+	and r2,ptrn
+	cmp r2,#0
+	moveq pinVal,#1
+	
 	mov pinNum,#16
-	mov pinVal,#1
 	bl SetGpio
-	mov delay,#0x3F0000
+
+	ldr delay,=500000
 	bl Timer
 
-b loop$
+	add seq,#1
+	cmp seq,#32
+	blt loop$
 
+// Turn LED off at the end.
+mov pinVal,#1
+mov pinNum,#16
+bl SetGpio
+
+.unreq pinNum
+.unreq pinVal
+.unreq delay
+
+.section .data
+	.align 2
+pattern:
+//	.int 0b11111111101010100010001000101010
+.int 0b00000000000000000000011100010101
